9b1c12bbea5af156c2c16b349b1bc6352d98617b
Elias Nikkinen <elias.nikkinen@tuni.fi>
2022-11-23 13:04:25 +0200

    fixed notation in add_departure

--- prg1/datastructures.hh
+++ prg1/datastructures.hh
@@ -165,7 +165,7 @@ public:
     // Short rationale for estimate: find is constant
     bool change_station_coord(StationID id, Coord newcoord);
 
-    // Estimate of performance: O(n²)
+    // Estimate of performance: O(n^2)
     // Short rationale for estimate: for loop and find are constant
     bool add_departure(StationID stationid, TrainID trainid, Time time);
 
